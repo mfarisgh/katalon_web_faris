a6623a75690c58721ba9ad9bd574ac99eaefc353
mfarisTB <farisfildza@mytestbits.com>
2024-03-17 21:22:52 +0800

    v2

---
+++ Test Suites/AutomationSuite.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AutomationSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5442bc37-447e-421e-8ee6-88f0fd3836a1</testSuiteGuid>
+   <testCaseLink>
+      <guid>8311dfdc-1979-4438-b6f6-7f63e7c1be67</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common/G0-S1-Login/G0-S1-TC1-Admin Successfully Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>73a582a7-8320-41a3-9c41-d3649189e63a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/G1-AdminUserManagement/G1-S1-SearchUsers/G1-S1-TC1-Users Enter Valid Username</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3c8fc56b-6dac-40bf-8b6f-f3599e892bdf</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a868ea18-94d2-4040-96ec-c98c079df6d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/G1-AdminUserManagement/G1-S1-SearchUsers/G1-S1-TC2-Users Enter Invalid Username</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>5fbf41f6-3025-4e52-9585-deaedebb96c3</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
